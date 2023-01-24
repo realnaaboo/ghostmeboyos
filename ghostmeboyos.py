01210317b9d981614a7542526beccb0d9347d88c
naaboo <114649147+realnaaboo@users.noreply.github.com>
2023-01-24 15:12:37 -0500

    Update ghostmeboyos.py

--- ghostmeboyos.py
+++ ghostmeboyos.py
@@ -4,9 +4,24 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-version = '0.2'
+version = '0.3'
+
+def checkVisibility(elem):
+    return WebDriverWait(browser, 10).until(EC.visibility_of(elem))
+def checkClickability(elem):
+    return WebDriverWait(browser, 10).until(EC.element_to_be_clickable(elem))
+# def checkPresenceOfElemID(elem):
+#     return WebDriverWait(browser, 30).until(EC.presence_of_element_located(By.ID, elem))
+# def checkPresenceOfElemCLASS(elem):
+#     return WebDriverWait(browser, 30).until(EC.presence_of_element_located(By.CLASS_NAME, elem))
+# def checkPresenceOfElemNAME(elem):
+#     WebDriverWait(browser, 30).until(EC.presence_of_element_located(By.NAME, elem)) 
+
+################# START #################
 
 print(f'\nGHOST ME BOYOS {version}\n')
+
+################# LOGIN #################
 
 username = input('Enter username: ') 
 password = input('Enter password: ')
@@ -17,14 +32,19 @@ browser.get(site)
 
 try: 
     user_field = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.NAME, 'username')))
-    user_field.send_keys(username)
-except: pass
+    if checkVisibility(user_field):
+        if checkClickability(user_field):
+            user_field.send_keys(username)
 
+except: pass
 
 try:
     pass_field = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.NAME, 'password')))
+    if checkVisibility(pass_field):
+        if checkClickability(pass_field):
+            pass_field.send_keys(password)
+
 except: pass
-pass_field.send_keys(password)
 
 
 # Click off "want 15% off?" popup
@@ -33,10 +53,16 @@ while i != 'Y':
     i = input('\nDid you remove 15% off popup? (Y/N) ')
 
 try:
-    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'combined-sign-in--button'))).click()
+    login = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'combined-sign-in--button')))
+    if checkVisibility(login):
+        if checkClickability(login):
+            login.click()
+            print('login successful...')
 except: pass
 
 time.sleep(3)
+
+################# USERID #################
 
 dark_mode = 'https://forum.bodybuilding.com/?styleid=63'
 browser.get(dark_mode)
@@ -48,6 +74,8 @@ except: pass
 id2 = id1.get_attribute('href')
 id3 = id2.split('=')
 userid = id3[1]
+
+################# POST HISTORY #################
 
 post_history = f'https://forum.bodybuilding.com/search.php?do=finduser&userid={userid}&contenttype=vBForum_Post&showposts=1'
 browser.get(post_history)
@@ -79,20 +107,32 @@ while True:
         window_name = browser.window_handles[-1] # focus last tab
         browser.switch_to.window(window_name=window_name)
         
-        try: # edits post for del
-            WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.NAME, z))).click()  
+        try: # edits post for del. targ -> "vB::QuickEdit::" + post ID
+            edit_post = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.NAME, z)))
+            if checkVisibility(edit_post):
+                if checkClickability(edit_post):
+                    edit_post.click()
         except: pass
 
         try: # deletes post
-            WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.ID, 'vB_Editor_QE_1_delete'))).click() 
+            delete_post = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.ID, 'vB_Editor_QE_1_delete'))) 
+            if checkVisibility(delete_post):
+                if checkClickability(delete_post):
+                    delete_post.click()
         except: pass
         
         try: # toggle 'delete message radio button'
-            WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'dep_ctrl'))).click() 
+            dlt_radio_toggle = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'dep_ctrl'))) 
+            if checkVisibility(dlt_radio_toggle):
+                if checkClickability(dlt_radio_toggle):
+                    dlt_radio_toggle.click()
         except: pass
 
         try: # confirm delete
-            WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.ID, 'quickedit_dodelete'))).click() 
+            confirm_delete = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.ID, 'quickedit_dodelete')))
+            if checkVisibility(confirm_delete):
+                if checkClickability(confirm_delete):
+                    confirm_delete.click()
         except: pass
         
         # close thread
@@ -110,9 +150,15 @@ while True:
     #     thread_links_elem = browser.find_elements(By.CLASS_NAME, 'posttitle [href]')
 
     try:
-        WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'prev_next [href]'))).click()
+        next_page = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'prev_next [href]')))
+        if checkVisibility(next_page):
+            if checkClickability(next_page):
+                next_page.click()
     except: pass
 
     try:
-        thread_links_elem = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'posttitle [href]')))
+        thread_links_elem = WebDriverWait(browser, 30).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'posttitle [href]')))
+        if checkVisibility(thread_links_elem):
+            if checkClickability(thread_links_elem):
+                thread_links_elem.click()
     except: pass
