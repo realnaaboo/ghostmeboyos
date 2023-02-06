bb7c0c7664f33034b7270cf9f765f8e57a12528e
naaboo <114649147+realnaaboo@users.noreply.github.com>
2023-02-06 18:10:28 -0500

    Update ghostmeboyos.py

--- ghostmeboyos.py
+++ ghostmeboyos.py
@@ -4,11 +4,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, ElementNotVisibleException, NoSuchElementException
 from selenium.common.exceptions import ElementClickInterceptedException
+# from dontclickonstream import getUserPass
 import time 
 import os   # to clear screen
 import sys  # to exit program 
 
-version = '0.6b'
+version = '0.7'
 
 def displayLogo():
     print(f'''
@@ -21,20 +22,11 @@ def displayLogo():
                                                                                                                
                                                                                                                
 ''')
-
-def checkVisibility(elem):
-    return WebDriverWait(browser, 10).until(EC.visibility_of(elem))
-def checkClickability(elem):
-    return WebDriverWait(browser, 10).until(EC.element_to_be_clickable(elem))
-
-################# START #################
-
-
-
 ################# LOGIN #################\
 
 
 def user_pass():
+    # username, password = getUserPass()
     username = password = False
     # Enter user/pass
     while not (username and password):
@@ -45,25 +37,39 @@ def user_pass():
         print('')
     return username, password
         
-def createFirefoxObject():
-    # Create Firefox object
-    return webdriver.Firefox()
+def chooseBrowser():
+    choice = False
+    while not (choice == 1 or choice == 2 or choice == 3):
+        os.system('cls')
+        displayLogo()
+
+        try: choice = int(input('\n1) Firefox\n2) Chrome\n3) Safari\n\nChoose browser: Chrome not working...\n-> '))        
+        except ValueError: pass
+    print('\nloading...')
+    match choice:
+        case 1:
+            return webdriver.Firefox()
+        case 2:
+            return webdriver.Chrome()
+        case 3: 
+            return webdriver.Safari()
 
 def load_signin(browser):
-    site = 'https://www.bodybuilding.com/combined-signin?referrer=https%3A%2F%2Fforum.bodybuilding.com%2F%23&country=US'
+    # site = 'https://www.bodybuilding.com/combined-signin?referrer=https%3A%2F%2Fforum.bodybuilding.com%2F%23&country=US'
+    site = 'https://forum.bodybuilding.com/?styleid=63'
     browser.get(site) 
 
 
 def user_pass_field(browser, username, password):
     # Search for username field, input username
     try: 
-        user_field = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.NAME, 'username')))
+        user_field = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, 'navbar_username')))
         user_field.send_keys(username)
     except ElementNotVisibleException: print('Username field not found...')
 
     # Search for password field, input password
     try:
-        pass_field = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.NAME, 'password')))
+        pass_field = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, 'navbar_password_hint')))
         pass_field.send_keys(password)
     except ElementNotVisibleException: print('Password field not found...')
             
@@ -76,13 +82,30 @@ def check_popup():
 def sign_in(browser):
     # Click 'sign-in' button
     try:
-        login = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'combined-sign-in--button'))).click()
+        WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'loginbutton'))).click()
     except ElementClickInterceptedException: print('Sign-in not reached...')
+
+def login_loop():
+    browser = False
+    id = False
+    while not id:
+        username, password = user_pass()
+        if not browser:
+            browser = chooseBrowser()
+        load_signin(browser)
+        user_pass_field(browser, username, password)
+        check_popup()
+        sign_in(browser)
+        time.sleep(3)   # Wait page load
+        # load_darkmode(browser)
+        id = get_userid(browser)
+    return browser, id
+
 
 # try:
 #     fifteenoff = WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.CLASS_NAME, 'ab-programmatic-close-button')))
 # except: pass
-# didclose = input('did id close?')
+# didclose = input('did it close?')
 
 ################# USERID #################
 
@@ -94,16 +117,21 @@ def load_darkmode(browser):
 def get_userid(browser):
     # Checks for user id (determines login success)
     try:
-        id1 = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'welcomelink [HREF]')))
+        id1 = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'welcomelink [HREF]')))
         print('Login successful...')
-    except NoSuchElementException: print('Login unsuccessful...')
+    except (NameError, NoSuchElementException, ElementNotVisibleException, TimeoutException): 
+        print('Login unsuccessful...')
+        time.sleep(3)
+        return False
 
     try:
         # Split id from link
         id2 = id1.get_attribute('href')
         id3 = id2.split('=')
         return id3[1]
-    except: print('uid not returned...')
+    except NameError: 
+        print('uid not returned...')
+        time.sleep(1.5)
 
 ################# POST HISTORY #################
 
@@ -140,61 +168,138 @@ def get_thread_links(browser):
 
 ################# PAGES + THREAD #################
 
-def thread_loop(browser, post_history_page_num):
-    global posts_deleted 
-    posts_deleted = 0   #stats
-    while True:
-        thread_links_list = get_thread_links(browser)
-        print(f'\nPAGE: [{post_history_page_num}]')
-        inner_thread_loop(browser, thread_links_list)
 
+################# MULT TABS #################
+def create_post_id_pairs(browser):
+    # Empty list for post pairs
+    link_post_id = []
+    # Get thread links
+    thread_links_list = get_thread_links(browser)
+    # Iterate through list
+    for link in thread_links_list:
+        post_link = link.get_attribute('href')
+        
+        # Split post ID from URL
+        x = post_link.split('post')
+        post_id = x[1]
+        
+        # concat to edit corresponding post ID
+        vB_post_id = 'vB::QuickEdit::' + post_id
+
+        post_dict = {f'link': post_link, 'postid': vB_post_id}
+        
+        link_post_id.append(post_dict)
+    return link_post_id
+
+def mult_tabs(browser):
+    while True:
+        link_post_id_pairs = create_post_id_pairs(browser)
+        pagecycle = 0
+        while pagecycle < len(link_post_id_pairs):
+            # choice = input('(Range 1-5)\nEnter # of tabs to open: ')
+            new_list = []
+            choice = 5
+            for count, pair in enumerate(link_post_id_pairs, start=1):
+
+                if count < len(link_post_id_pairs):
+                    isLast = False
+                else: isLast = True
+
+                new_pair = {'link': pair['link'], 'postid': pair['postid']}
+                new_list.append(new_pair) # new_pair.items?
+                
+                browser.execute_script(f'''window.open("{pair['link']}", "_blank");''')
+                pagecycle += 1
+                time.sleep(1)
+                
+                if ((count > 0) and (count % choice == 0)):
+                    
+                    time.sleep(3)
+                    # Get window name of last tab
+                    thread_loop(browser, new_list, count)
+                    new_list = []
+
+                elif (count % choice-1 or choice-2 or choice-3 or choice-4 == 0) and (isLast == True): #4
+                    
+                    time.sleep(3)
+                    # Get window name of last tab
+                    thread_loop(browser, new_list, count)
+                    new_list = []
+
+                # elif (count % choice-2 == 0) and (isLast == True): #3
+
+                #     time.sleep(3)
+                #     # Get window name of last tab
+                #     thread_loop(browser, new_list, count)
+                #     new_list = []
+
+                # elif (count % choice-3 == 0) and (isLast == True): #2
+
+                #     time.sleep(3)
+                #     # Get window name of last tab
+                #     thread_loop(browser, new_list, count)                              
+                #     new_list = []
+
+                # elif (count % choice-4 == 0) and (isLast == True): #1
+
+                #     time.sleep(3)
+                #     # Get window name of last tab
+                #     thread_loop(browser, new_list, count)
+                #     new_list = []
+
+            break  
+            # if pagecycle > len(link_postid_pairs):
+            #     break
+        check_next_page(browser)
+
+def check_next_page(browser):
+        global post_history_page_num
         try: # next page   
             next_page_list = WebDriverWait(browser, 5).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, 'prev_next [href]')))
             next_page = next_page_list[1]
             next_page.click()
+            # update page num
+            post_history_page_num = str(int(post_history_page_num) + 1)
         except (ElementNotVisibleException, NoSuchElementException): 
             print('Next page not found...')
             input('Press enter to close program.\n')
             sys.exit()
 
-        # update page num
-        post_history_page_num = str(int(post_history_page_num) + 1)
 
-def inner_thread_loop(browser, thread_links_list):
-    for count, x in enumerate(thread_links_list, start=0):
-        
-        # For each item in thread_list, GET 'HREF' attr
-        thread_link = x.get_attribute('href')
+def thread_loop(browser, new_list, count):
+    global post_history_page_num
+    print(f'\nPAGE: [{post_history_page_num}]')
+    inner_thread_loop(browser, new_list, count)
 
-        # Split post ID from URL
-        x = thread_link.split('post')
-        y = x[1]
+def inner_thread_loop(browser, new_list, count):
+    for count, x in enumerate(new_list, start=0):
         
-        # concat to edit corresponding post ID
-        z = 'vB::QuickEdit::' + y
-        
-        browser.execute_script(f'''window.open("{thread_link}", "_blank");''')
-
         window_name = browser.window_handles[-1] # focus last tab
         browser.switch_to.window(window_name=window_name)
 
         # check if locked thread
         thread_status = WebDriverWait(browser, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, 'newcontent_textcontrol')))
-        if thread_status.text == 'Closed Thread':
+        if thread_status.text == 'Closed Thread' and count == 4:
+            print('Thread locked.')
+            browser.close()
+            window_name = browser.window_handles[0] 
+            browser.switch_to.window(window_name=window_name)
+            break
+        elif thread_status.text == 'Closed Thread':
             print('Thread locked.')
             browser.close()
             # focus first tab
             window_name = browser.window_handles[0] 
             browser.switch_to.window(window_name=window_name)
             continue
-        
+
         try: # edits post for del. targ -> "vB::QuickEdit::" + post ID
-            edit_post = WebDriverWait(browser, 60).until(EC.element_to_be_clickable((By.NAME, z))).click()
+            WebDriverWait(browser, 60).until(EC.element_to_be_clickable((By.NAME, x['postid']))).click()
         except ElementClickInterceptedException: 
             print('Unable to click edit button...')
 
         try: # deletes post
-            delete_post = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'vB_Editor_QE_1_delete'))).click() 
+            WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'vB_Editor_QE_1_delete'))).click() 
         except ElementClickInterceptedException: 
             print('Unable to click delete button...')
         except TimeoutException:
@@ -204,15 +309,16 @@ def inner_thread_loop(browser, thread_links_list):
             # focus first tab
             window_name = browser.window_handles[0] 
             browser.switch_to.window(window_name=window_name)
-            continue
+            if (x == new_list[-1]): break
+            else: continue
         
         try: # toggle 'delete message radio button'
-            dlt_radio_toggle = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'dep_ctrl'))).click() 
+            WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'dep_ctrl'))).click() 
         except ElementClickInterceptedException: 
             print('Unable to click toggle delete button...')
 
         try: # confirm delete
-            confirm_delete = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'quickedit_dodelete'))).click()
+            WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'quickedit_dodelete'))).click()
         except ElementClickInterceptedException: 
             print('Unable to click confirm delete button...')
 
@@ -227,22 +333,22 @@ def inner_thread_loop(browser, thread_links_list):
         window_name = browser.window_handles[0] 
         browser.switch_to.window(window_name=window_name)
 
-
+################# START #################
 
 def main():
-    username, password = user_pass()
-    browser = createFirefoxObject() 
-    load_signin(browser)
-    user_pass_field(browser, username, password)
-    check_popup()
-    sign_in(browser)
-    time.sleep(3)   # Wait page load
-    load_darkmode(browser)
-    user_id = get_userid(browser)
+    '''
+        main function: this runs the program
+        for MISC users, read each line to understand how it works.  
+    '''
+    global post_history_page_num
+    global posts_deleted 
+    posts_deleted = 0   #stats
+    browser, user_id = login_loop()
     load_posthistory(browser, user_id)
     search_id = get_search_id(browser)
     post_history_page, post_history_page_num = get_post_history_page(search_id)
     load_post_history_page_check(browser, post_history_page, post_history_page_num)
-    thread_loop(browser, post_history_page_num)
+    # thread_loop(browser, post_history_page_num)
+    mult_tabs(browser)
 
 main()
